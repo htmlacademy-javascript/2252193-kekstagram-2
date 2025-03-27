e1a751caaba7454eaa6421cf19a7953edb2d62f8
Maria Gromova <businca99@list.ru>
2025-03-27 20:38:39 +0300

    исправлена ошибка

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import {getPhotoArray} from './post-maker.js';
-import {renderThumbs, initThumbsListener} from './thumbnails-maker.js';
+import {renderThumbs, initThumbsListener} from './thumbnail-maker.js';
 
 const postsData = getPhotoArray();
 renderThumbs(postsData);
-initThumbsListener(postsData); 
+initThumbsListener(postsData);
