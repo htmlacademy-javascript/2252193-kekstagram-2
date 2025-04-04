f43a908dd48f0ca4ff66684395b9d7f7a762b6aa
ShaggyMary <businca99@list.ru>
2025-04-04 18:05:21 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import {getPhotoArray} from './post-maker.js';
 import {renderThumbs, initThumbsListener} from './thumbnail-maker.js';
-import {initUploadModal} from './upload-photo-form';
-import './validation-form';
+import {initUploadModal} from './upload-photo-form.js';
+import './validation-form.js';
 
 const postsData = getPhotoArray();
 
