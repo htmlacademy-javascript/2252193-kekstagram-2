32d1b342d17639bbfd9f7a25f8e01a39ba7fecb0
ShaggyMary <businca99@list.ru>
2025-04-16 22:34:55 +0300

    Update server-api.js

--- js/server-api.js
+++ js/server-api.js
@@ -1,22 +1,6 @@
 import { showAlertMessage } from './message-upload-popup.js';
 import { API_URL } from './endpoints.js';
 
-// const getDataFromServer = (onSuccess) => {
-//   fetch(`${API_URL}/data`)
-//     .then((response) => {
-//       if (response.ok) {
-//         return response.json();
-//       }
-//       throw new Error(`${response.status} ${response.statusText}`);
-//     })
-//     .then((posts) => {
-//       onSuccess(posts);
-//     })
-//     .catch(() => {
-//       showAlertMessage('Не удалось загрузить данные с сервера');
-//     });
-// };
-
 const getDataFromServer = async (onSuccess) => {
   try {
     const response = await fetch(`${API_URL}/data`);
